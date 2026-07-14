b744868b8826af73bca20b88b128b196b16ccc5a
James <escuderojames12@gmail.com>
2026-07-13 22:15:05 -0700

    feat: implemented a route table to enter through the internet gateway

--- main.tf
+++ main.tf
@@ -22,6 +22,20 @@ resource "aws_internet_gateway" "internet_gateway" {
     }
 }
 
+# Creating our route table to our public-subnet
+resource "aws_route_table" "route_table" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.internet_gateway.id
+    }
+
+    tags = {
+        Name = "internet_gateway_route_table"
+    }
+}
+
 # Creating our public subnet
 resource "aws_subnet" "public" {
     vpc_id = aws_vpc.main.id
