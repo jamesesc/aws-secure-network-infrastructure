e46e556b508857f48ad3b437cd855cbc1b37b0dc
James <escuderojames12@gmail.com>
2026-07-13 21:08:32 -0700

    doc: added comments to main

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-
+# Terraform auth connection/configuation for our AWS account
 provider "aws" {
     region     = "us-west-2"
 }
 
+# Creating our main VPC
 resource "aws_vpc" "main" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
