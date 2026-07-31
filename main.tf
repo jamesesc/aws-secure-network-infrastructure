77d6e778555ed5e5432af744a7513773ab299312
James <escuderojames12@gmail.com>
2026-07-30 19:36:01 -0700

    doc: add comments and fixed the naming for our private ec2

--- main.tf
+++ main.tf
@@ -110,13 +110,13 @@ resource "aws_vpc_security_group_egress_rule" "allowed_traffic_private_egress" {
     ip_protocol = "-1"
 }
 
+# Attaching the public ssh key to our AWS to our EC2 instance
 resource "aws_key_pair" "ec2_access_key" {
     key_name = "access_key"
     public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOubr3w9Fo7q1u2Ijj2CciI6yqUoPavOHjkE41I0Pwkl first-cloud-project"
 }
 
-
-
+# Filtering and storing our EC2 AMI data reference
 data "aws_ami" "ec2_ami" {
     most_recent = true
     owners = ["amazon"]
@@ -142,7 +142,8 @@ data "aws_ami" "ec2_ami" {
     }
 }
 
-resource "aws_instance" "example" {
+# Creating our private ec2
+resource "aws_instance" "private_ec2" {
     ami = data.aws_ami.ec2_ami.id
     instance_type = "t2.micro"
     subnet_id = aws_subnet.private.id
@@ -152,7 +153,7 @@ resource "aws_instance" "example" {
     key_name = aws_key_pair.ec2_access_key.key_name
 
     tags = {
-        Name = "EC2-Version1"
+        Name = "private-EC2"
     }
 }
 
